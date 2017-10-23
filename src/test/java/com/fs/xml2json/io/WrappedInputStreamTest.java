bd4cf556a7c3c32ec847722ffa4e765c7a74ed7d
Anton <amykolaienko@gmail.com>
2017-10-23 23:59:21 +0300

    Added test

--- src/test/java/com/fs/xml2json/io/WrappedInputStreamTest.java
+++ src/test/java/com/fs/xml2json/io/WrappedInputStreamTest.java
@@ -87,7 +87,37 @@ public class WrappedInputStreamTest {
         assertEquals(bytesToReadBeforeCancel, bytesSendToListener.get());
     }
     
-    
+    @Test
+    public void testCancelReadBuffer() throws IOException {
+        File sourceFile = new File(this.getClass().getClassLoader().getResource("SampleJson.json").getFile());
+        System.out.println(sourceFile.getAbsolutePath());
+        assertTrue(sourceFile.exists());
+        
+        AtomicBoolean isCanceled = new AtomicBoolean(false);
+        AtomicLong bytesRead = new AtomicLong(0);
+        AtomicLong bytesSendToListener = new AtomicLong(0);
+        IFileReadListener listener = new CustomFileReadListener(bytesSendToListener);
+        
+        int bytesToReadBeforeCancel = (int)sourceFile.length()/2;
+        byte[] buffer = new byte[128];
+
+        try (InputStream in = new WrappedInputStream(new FileInputStream(sourceFile), listener, isCanceled)){
+            while (in.available() > 0) {
+                if (bytesToReadBeforeCancel == bytesRead.get()) {
+                    isCanceled.compareAndSet(false, true);
+                }
+                int bytesToRead = (bytesRead.get() + buffer.length) >= bytesToReadBeforeCancel 
+                        ? (bytesToReadBeforeCancel - bytesRead.intValue()) : buffer.length;
+                int bytes = in.read(buffer, 0, bytesToRead);
+                if (bytes > 0) {
+                    bytesRead.addAndGet(bytes);
+                }
+            }
+        }
+
+        assertEquals(bytesToReadBeforeCancel, bytesRead.get());
+        assertEquals(bytesToReadBeforeCancel, bytesSendToListener.get());
+    }
     
     private class CustomFileReadListener implements IFileReadListener {
 
