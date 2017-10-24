bb59b1dc88a2e4c8bc0c0f1612f74b5181957db9
Anton Mykolaienko <amykolaienko@gmail.com>
2017-10-24 22:27:51 +0300

    Added POJOs

---
+++ src/test/java/com/fs/xml2json/model/ComplexObject.java
@@ -0,0 +1,14 @@
+
+package com.fs.xml2json.model;
+
+/**
+ *
+ * @author Anton Mykolaienko
+ * @since 1.2.0
+ */
+public class ComplexObject {
+    
+    private String text;
+    private Integer intValue;
+    
+}
---
+++ src/test/java/com/fs/xml2json/model/SimpleObject.java
@@ -0,0 +1,11 @@
+
+package com.fs.xml2json.model;
+
+/**
+ *
+ * @author Anton Mykolaienko
+ * @since 1.2.0
+ */
+public class SimpleObject {
+    
+}
