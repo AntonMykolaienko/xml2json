403b58bf99dbb4e0cfd5eccb5b09ae525ea833c5
Anton Mykolaienko <amykolaienko@gmail.com>
2017-11-01 23:46:31 +0200

    Added test

--- src/test/java/com/fs/xml2json/util/ApplicationUtilsTest.java
+++ src/test/java/com/fs/xml2json/util/ApplicationUtilsTest.java
@@ -3,6 +3,12 @@ package com.fs.xml2json.util;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.MockGateway;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
  * Tests for ApplicationUtils class.
@@ -10,6 +16,8 @@ import org.junit.Test;
  * @author Anton Mykolaienko
  * @since 1.2.0
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ApplicationUtils.class)
 public class ApplicationUtilsTest {
 
 
@@ -17,4 +25,21 @@ public class ApplicationUtilsTest {
     public void testApplicationVersion() {
         Assert.assertEquals(ApplicationUtils.UNNOWN_VERSION, ApplicationUtils.getVersion());
     }
+    
+    
+    @Test
+    public void getImplementationVersion() throws Exception {
+        PowerMockito.spy(ApplicationUtils.class);
+        PowerMockito.doReturn("1.0.0").when(ApplicationUtils.class, "getVersionFromManifest");
+
+        String s = ApplicationUtils.getVersion();
+        
+        // PowerMock, verify
+        PowerMockito.verifyStatic(Package.class);
+        
+        // Assert
+        Assert.assertEquals("1.0.0", s);
+    }
+
+    
 }
