2798393deeff4b2f7b77f5477288d2b6479e3538
Anton <amykolaienko@gmail.com>
2017-11-03 00:45:19 +0200

    Added tests

--- src/test/java/com/fs/xml2json/CMDStarterMockTest.java
+++ src/test/java/com/fs/xml2json/CMDStarterMockTest.java
@@ -72,8 +72,8 @@ public class CMDStarterMockTest {
     
     
     @Test
-    @Ignore
-    public void testStartNoGuiAndAnswerNo() throws FileNotFoundException, IOException {
+    @PrepareForTest({Starter.class, LoggerFactory.class})
+    public void testStartNoGuiAndAnswerNo() throws FileNotFoundException, IOException, Exception {
         File sourceFolder = new File("src/test/resources/xml");
         
         BufferedReader bufferedReader = Mockito.mock(BufferedReader.class);
@@ -84,54 +84,12 @@ public class CMDStarterMockTest {
         
         String[] args = new String[]{"--noGui", "--sourceFolder", sourceFolder.getAbsolutePath(), 
             "--destinationFolder", destinationFolder.getAbsolutePath(), "--pattern", "*.xml"};
-        Starter.main(args);
-        
-        Assert.assertTrue(destinationFolder.exists());
-        Assert.assertEquals(2, destinationFolder.listFiles().length);
-        Stream.of(destinationFolder.listFiles()).forEach(file -> Assert.assertTrue(file.length() > 0));
-        
-        Stream.of(destinationFolder.listFiles()).forEach(file -> {
-            try (FileChannel outChan = new FileOutputStream(file, true).getChannel()) {
-                outChan.truncate(0);
-            } catch (IOException ex) {}
-        });
-        
-        Starter starterClass = Mockito.mock(Starter.class);
-        InputStream in = Mockito.mock(InputStream.class);
-        InputStreamReader inr = Mockito.mock(InputStreamReader.class);
-        try {
-            PowerMockito.whenNew(Starter.class).withArguments(args).thenReturn(starterClass);
-            PowerMockito.whenNew(InputStreamReader.class).withArguments(in).thenReturn(inr);
-            PowerMockito.whenNew(BufferedReader.class).withArguments(inr).thenReturn(bufferedReader);
-            PowerMockito.when(bufferedReader.readLine()).thenReturn("r").thenReturn("n");
-            //Whitebox.invokeMethod(starterClass, "MethodName", arguement);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        Starter starter = new Starter(args);
-        //Starter.main(args);
-        starter.start();
-        
-        Assert.assertEquals(2, destinationFolder.listFiles().length);
-        Stream.of(destinationFolder.listFiles()).forEach(file -> Assert.assertTrue(file.length() == 0));
-    }
-    
-    
-    @Test
-    @Ignore
-    public void testStartNoGuiAndAnswerYes() throws FileNotFoundException, IOException {
-        File sourceFolder = new File("src/test/resources/xml");
-        
-        BufferedReader bufferedReader = Mockito.mock(BufferedReader.class);
-        BDDMockito.when(bufferedReader.readLine()).thenReturn("r").thenReturn("Y");
-        
-        System.out.println("SourceFolder: " + sourceFolder.getAbsolutePath());
-        Assert.assertTrue(sourceFolder.exists());
-        
-        String[] args = new String[]{"--noGui", "--sourceFolder", sourceFolder.getAbsolutePath(), 
-            "--destinationFolder", destinationFolder.getAbsolutePath(), "--pattern", "*.xml"};
-        Mockito.mock(Starter.class);
-        //Starter.main(args);
+
+        PowerMockito.mockStatic(LoggerFactory.class);
+        Logger logger = PowerMockito.mock(Logger.class);
+        PowerMockito.when(LoggerFactory.getLogger(Mockito.any(Class.class))).thenReturn(logger);
+
+        // first run
         Starter starter = Mockito.spy(new Starter(args));
         starter.start();
         
@@ -145,23 +103,60 @@ public class CMDStarterMockTest {
             } catch (IOException ex) {}
         });
         
-        Starter starterClass = Mockito.mock(Starter.class);
+        Starter starterClass = Mockito.spy(new Starter(args));
+        PowerMockito.whenNew(BufferedReader.class).withAnyArguments().thenReturn(bufferedReader);
+        
+        starterClass.start();       
+        
+        Assert.assertEquals(2, destinationFolder.listFiles().length);
+        Stream.of(destinationFolder.listFiles()).forEach(file -> Assert.assertTrue(file.length() == 0));
+    }
+    
+    
+    @Test
+    @PrepareForTest({Starter.class, LoggerFactory.class})
+    public void testStartNoGuiAndAnswerYes() throws FileNotFoundException, IOException, Exception {
+        File sourceFolder = new File("src/test/resources/xml");
+        
+        BufferedReader bufferedReader = Mockito.mock(BufferedReader.class);
+        BDDMockito.when(bufferedReader.readLine()).thenReturn("r").thenReturn("Y");
+        
+        System.out.println("SourceFolder: " + sourceFolder.getAbsolutePath());
+        Assert.assertTrue(sourceFolder.exists());
+        
+        String[] args = new String[]{"--noGui", "--sourceFolder", sourceFolder.getAbsolutePath(), 
+            "--destinationFolder", destinationFolder.getAbsolutePath(), "--pattern", "*.xml"};
+        
+        PowerMockito.mockStatic(LoggerFactory.class);
+        Logger logger = PowerMockito.mock(Logger.class);
+        PowerMockito.when(LoggerFactory.getLogger(Mockito.any(Class.class))).thenReturn(logger);
+        
+        Starter starter = Mockito.spy(new Starter(args));
+        starter.start();
+        
+        Assert.assertTrue(destinationFolder.exists());
+        Assert.assertEquals(2, destinationFolder.listFiles().length);
+        Stream.of(destinationFolder.listFiles()).forEach(file -> Assert.assertTrue(file.length() > 0));
+        
+        Stream.of(destinationFolder.listFiles()).forEach(file -> {
+            try (FileChannel outChan = new FileOutputStream(file, true).getChannel()) {
+                outChan.truncate(0);
+            } catch (IOException ex) {}
+        });
+        
+//        Starter starterClass = Mockito.mock(Starter.class);
         //InputStream in = Mockito.mock(InputStream.class);
-        BufferedInputStream in = Mockito.mock(BufferedInputStream.class);
-        InputStreamReader inr = Mockito.mock(InputStreamReader.class);
-        try {
-            PowerMockito.whenNew(Starter.class).withArguments(args).thenReturn(starterClass);
-            PowerMockito.whenNew(InputStreamReader.class).withArguments(in).thenReturn(inr);
-            PowerMockito.whenNew(BufferedReader.class).withArguments(inr).thenReturn(bufferedReader);
-            PowerMockito.when(bufferedReader.readLine()).thenReturn("r").thenReturn("Y");
+//        BufferedInputStream in = Mockito.mock(BufferedInputStream.class);
+//        InputStreamReader inr = Mockito.mock(InputStreamReader.class);
+//        PowerMockito.whenNew(Starter.class).withArguments(args).thenReturn(starterClass);
+//        PowerMockito.whenNew(InputStreamReader.class).withArguments(in).thenReturn(inr);
+//        PowerMockito.whenNew(BufferedReader.class).withArguments(inr).thenReturn(bufferedReader);
+//        PowerMockito.when(bufferedReader.readLine()).thenReturn("r").thenReturn("Y");
 //            //Whitebox.invokeMethod(starterClass, "start");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
         
         
         Starter secondStarter = Mockito.spy(new Starter(args));
-
+        PowerMockito.whenNew(BufferedReader.class).withAnyArguments().thenReturn(bufferedReader);
         
 //        //Starter.main(args);
         secondStarter.start();
@@ -173,7 +168,6 @@ public class CMDStarterMockTest {
     
     @Test
     @PrepareForTest({Starter.class, GuiStarter.class, LoggerFactory.class})
-    //@Ignore
     public void testStartWithFakeGui() throws Exception {
         String[] args = new String[]{};
         //given
