7079fb518ac6750fbf872701424e06b9dabbd2f9
Anton <amykolaienko@gmail.com>
2017-11-07 01:26:53 +0200

    Checkstyle fixes

--- src/main/java/com/fs/xml2json/controller/HostServicesProvider.java
+++ src/main/java/com/fs/xml2json/controller/HostServicesProvider.java
@@ -1,21 +1,22 @@
 /**
  * Copyright © 2016-2017 Anton Mykolaienko. All rights reserved. Contacts: <amykolaienko@gmail.com>
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * 
- *  http://www.apache.org/licenses/LICENSE-2.0 
- *  
- *  Unless required by applicable law or agreed to in writing, software 
- *  distributed under the License is distributed on an "AS IS" BASIS, 
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- *  See the License for the specific language governing permissions and 
- *  limitations under the License. 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package com.fs.xml2json.controller;
 
+import javafx.application.Application;
 import javafx.application.HostServices;
 
 /**
@@ -26,21 +27,37 @@ import javafx.application.HostServices;
  */
 public class HostServicesProvider {
 
+    /**
+     * Singleton instance of {@link HostServicesProvider}.
+     */
     public static final HostServicesProvider INSTANCE = new HostServicesProvider();
 
-    private HostServices hostServices ;
-    
+    /**
+     * Real HostServices.
+     */
+    private HostServices hostServices;
+
+    /**
+     * Initialize class with value.
+     * 
+     * @param hostServices instance of {@link HostServices} from {@link Application}
+     */
     public void init(HostServices hostServices) {
         if (this.hostServices != null) {
             throw new IllegalStateException("Host services already initialized");
         }
-        this.hostServices = hostServices ;
+        this.hostServices = hostServices;
     }
-    
+
+    /**
+     * Returns instance of {@link HostServices}.
+     * 
+     * @return {@link HostServices}
+     */
     public HostServices getHostServices() {
         if (hostServices == null) {
             throw new IllegalStateException("Host services not initialized");
         }
-        return hostServices ;
+        return hostServices;
     }
 }
